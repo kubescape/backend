07e167c7634ed8413623ba0827a7328ec080d776
rinao12 <121787861+rinao12@users.noreply.github.com>
2024-07-08 10:32:24 +0300

    SUB-4890 - [KYOS] As a User I want to be able to accept the risk of CVE/Resource for all Cluster, Namespace, Workload, Container
    Signed-off-by: rinao12 <121787861+rinao12@users.noreply.github.com>

--- pkg/client/v1/vulnerabilities_test.go
+++ pkg/client/v1/vulnerabilities_test.go
@@ -1,11 +1,24 @@
 package v1
 
 import (
+	url2 "net/url"
 	"testing"
 
 	"github.com/armosec/armoapi-go/identifiers"
 	"github.com/stretchr/testify/assert"
 )
+
+func Test_getCVEExceptionsURLByRawQuery(t *testing.T) {
+	url, err := getCVEExceptionsURLByRawQuery("http://localhost:8080", "abc", &url2.Values{
+		"scope.namespace": []string{"kube-system", "*/*"},
+		"scope.cluster":   []string{"c1", "c2"},
+		"scope.name":      []string{"n1", "*/*"},
+		"scope.kind":      []string{"deployment"},
+		"scope.other":     []string{""},
+	})
+	assert.NoError(t, err)
+	assert.Equal(t, "http://localhost:8080/api/v1/armoVulnerabilityExceptions?customerGUID=abc&scope.cluster=c1&scope.cluster=c2&scope.kind=deployment&scope.name=n1&scope.name=%2A%2F%2A&scope.namespace=kube-system&scope.namespace=%2A%2F%2A&scope.other=", url.String())
+}
 
 func Test_getCVEExceptionsURL(t *testing.T) {
 	url, err := getCVEExceptionsURL("http://localhost:8080", "abc", &identifiers.PortalDesignator{
