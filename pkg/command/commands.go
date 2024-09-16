6d66c53aa1d94d1f190f555b6b2b8600f3b75c44
Amir Malka <amirm@armosec.io>
2024-09-16 14:33:00 +0300

    fixes
    Signed-off-by: Amir Malka <amirm@armosec.io>

---
+++ pkg/command/commands.go
@@ -0,0 +1,27 @@
+package command
+
+type OperatorCommandType string
+
+const (
+	OperatorCommandNodeNameLabelKey string = "nodeName" // holds the node name label, on which to run the command (optional)
+
+	// command types will be defined here
+	OperatorCommandTypeResponse OperatorCommandType = "response"
+)
+
+// ResponseCommand
+type ResponseAction string
+
+const (
+	ResponseActionKill  ResponseAction = "Kill"
+	ResponseActionStop  ResponseAction = "Stop"
+	ResponseActionPause ResponseAction = "Pause"
+)
+
+type ResponseCommand struct {
+	Namespace     string         `json:"namespace,omitempty"`
+	PodName       string         `json:"podName,omitempty"`
+	ContainerName string         `json:"containerName,omitempty"`
+	Pid           *uint32        `json:"pid,omitempty"`
+	Action        ResponseAction `json:"action,omitempty"`
+}
